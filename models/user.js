81c36572eb7858e1e671dc393f7aac02d2fd3b6c
Sofia Ex <dev.exarchou@outlook.com>
2024-11-05 18:22:19 +0100

    use database for users

--- models/user.js
+++ models/user.js
@@ -1,7 +1,10 @@
-import { model } from "mongoose"
+import { model, Schema } from "mongoose"
 
-//...
-
-const user = new Schema({})
+const user = new Schema({
+    firstName: { type: String, require: true },
+    lastName: { type: String, require: true },
+    userName: { type: String, require: true },
+    country: { type: String, require: false },
+})
 
 export const User = model("User", user)
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,100 +1,43 @@
 import { Router } from "express"
 import { validateUser } from "../middlewares/validate-user.js"
+import { User } from "../models/user.js"
 
 export const userRouter = Router()
 
-let usersCollection = [
-    {
-        id: 0,
-        firstName: "Sonia",
-        lastName: "Vecchia",
-        country: "Italy",
-        username: "Sunshine62",
-    },
-    {
-        id: 1,
-        firstName: "Maria",
-        lastName: "Rossi",
-        country: "Italy",
-        username: "MiaRossi87",
-    },
-    {
-        id: 2,
-        firstName: "Linda",
-        lastName: "Schmidt",
-        country: "Germany",
-        username: "LindieS21",
-    },
-    {
-        id: 3,
-        firstName: "Emma",
-        lastName: "Brown",
-        country: "USA",
-        username: "EmmyB99",
-    },
-    {
-        id: 4,
-        firstName: "Sophia",
-        lastName: "Garcia",
-        country: "Spain",
-        username: "SophieGar12",
-    },
-    {
-        id: 5,
-        firstName: "Olivia",
-        lastName: "Martinez",
-        country: "Mexico",
-        username: "LivMartinez44",
-    },
-    {
-        id: 6,
-        firstName: "Hana",
-        lastName: "Yamada",
-        country: "Japan",
-        username: "HanaYam55",
-    },
-    {
-        id: 7,
-        firstName: "Ava",
-        lastName: "Smith",
-        country: "Australia",
-        username: "AvaSmitty08",
-    },
-    {
-        id: 8,
-        firstName: "Chloe",
-        lastName: "Dupont",
-        country: "France",
-        username: "ChloeD76",
-    },
-    {
-        id: 9,
-        firstName: "Nora",
-        lastName: "Johansen",
-        country: "Norway",
-        username: "NoraJHansen33",
-    },
-]
-
-userRouter.get("/", (req, res) => {
-    return res.status(200).json(usersCollection)
+userRouter.get("/", async (req, res) => {
+    const users = await User.find({})
+    return res.status(200).json(users)
 })
 
-userRouter.get("/:id", (req, res) => {
-    console.log("get user by id", req.params.id)
+userRouter.get("/:id", async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id).exec()
 
-    const rs = usersCollection.find((user) => user.id === req.params.id * 1)
-    if (rs) return res.status(200).json(rs)
-    else return res.status(404).json()
+        if (user) {
+            return res.status(200).json(user)
+        } else {
+            return res.status(404).json({ message: "User not found" })
+        }
+    } catch (error) {
+        return res
+            .status(500)
+            .json({ message: "Internal Server Error", error: error.message })
+    }
 })
 
-userRouter.post("/", validateUser, (req, res) => {
+userRouter.post("/", validateUser, async (req, res) => {
     const user = req.body
-    console.log("firstName", user.firstName)
 
-    usersCollection.push({
-        id: usersCollection.length + 1,
-        ...user,
-    })
-    return res.status(201).json()
+    try {
+        const newUser = new User({
+            ...user,
+        })
+        await newUser.save()
+
+        return res.status(201).json({ id: newUser._id })
+    } catch (error) {
+        return res
+            .status(500)
+            .json({ message: "Internal Server Error", error: error.message })
+    }
 })
