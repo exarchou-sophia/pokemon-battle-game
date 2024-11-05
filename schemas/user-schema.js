78c4b8e9fc87d31068e8339f90508c37e812762a
Sofia Ex <dev.exarchou@outlook.com>
2024-11-05 22:57:15 +0100

    fix missing username

--- schemas/user-schema.js
+++ schemas/user-schema.js
@@ -3,6 +3,6 @@ import Joi from "joi"
 export const userSchema = Joi.object({
     firstName: Joi.string().min(2).max(25).required(),
     lastName: Joi.string().min(2).max(25).required(),
+    userName: Joi.string().alphanum().min(3).max(30).required(),
     country: Joi.string().min(2).max(20).required(),
-    username: Joi.string().alphanum().min(3).max(30).required(),
 })
