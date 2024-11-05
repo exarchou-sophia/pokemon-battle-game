2a80024c4c0b2f8a9a18084fe029cfc00fd5331e
Sofia Ex <dev.exarchou@outlook.com>
2024-11-05 20:51:49 +0100

    create leaderboard

--- routes/leaderboard-routes.js
+++ routes/leaderboard-routes.js
@@ -1,81 +1,51 @@
 import { Router } from "express"
-import { validateLeaderboard } from "../middlewares/validate-leaderboard.js"
-import { LeaderBoard } from "../models/leader-board.js"
+import { BattleOutcome } from "../models/battle-outcome.js"
 
 export const leaderboardRouter = Router()
 
-// const leaderboardCollection = [
-//     {
-//         id: 0,
-//         country: "Italy",
-//         username: "Sunshine62",
-//         score: 1450,
-//         dateCreated: "2024-11-01T10:00:00Z",
-//     },
-//     {
-//         id: 1,
-//         country: "Italy",
-//         username: "MiaRossi87",
-//         score: 1520,
-//         dateCreated: "2024-11-02T15:30:00Z",
-//     },
-//     {
-//         id: 2,
-//         country: "Germany",
-//         username: "LindieS21",
-//         score: 1380,
-//         dateCreated: "2024-11-03T12:45:00Z",
-//     },
-//     {
-//         id: 3,
-//         country: "USA",
-//         username: "EmmyB99",
-//         score: 1600,
-//         dateCreated: "2024-11-04T09:20:00Z",
-//     },
-//     {
-//         id: 4,
-//         country: "Spain",
-//         username: "SophieGar12",
-//         score: 1490,
-//         dateCreated: "2024-11-01T20:15:00Z",
-//     },
-//     {
-//         id: 5,
-//         country: "Mexico",
-//         username: "LivMartinez44",
-//         score: 1575,
-//         dateCreated: "2024-11-02T18:40:00Z",
-//     },
-// ]
+leaderboardRouter.get("/", async (_, res) => {
+    const summary = await BattleOutcome.aggregate([
+        {
+            $facet: {
+                redPlayers: [
+                    {
+                        $project: {
+                            playerId: "$playerRed.playerId",
+                            score: "$playerRed.score",
+                        },
+                    },
+                ],
+                bluePlayers: [
+                    {
+                        $project: {
+                            playerId: "$playerBlue.playerId",
+                            score: "$playerBlue.score",
+                        },
+                    },
+                ],
+            },
+        },
+        {
+            $project: {
+                allPlayers: { $concatArrays: ["$redPlayers", "$bluePlayers"] },
+            },
+        },
+        { $unwind: "$allPlayers" },
+        {
+            $group: {
+                _id: "$allPlayers.playerId",
+                totalScore: { $sum: "$allPlayers.score" },
+            },
+        },
+        {
+            $project: {
+                _id: 0,
+                playerId: "$_id",
+                score: "$totalScore",
+            },
+        },
+        { $sort: { score: -1 } },
+    ])
 
-leaderboardRouter.get("/", (req, res) => {
-    return res.status(200).json(LeaderBoard)
-})
-
-leaderboardRouter.get("/:id", async (req, res) => {
-    try {
-        const rs = await LeaderBoard.findById(req.params.id).andexec()
-        // (user) => user.id === req.params.id * 1,
-        if (rs) return res.status(200).json(rs)
-        else return res.status(404).json()
-    } catch (error) {
-        return res
-            .status(500)
-            .json({ message: "Internal Server Error", error: error.message })
-    }
-})
-
-leaderboardRouter.post("/", validateLeaderboard, async (req, res) => {
-    const leaderboard = req.body
-    try {
-        const newLeaderboard = new LeaderBoard({
-            ...leaderboard,
-        })
-        return res.status(201).json()
-    } catch (error) {
-        return res
-            .status(500)
-            .json({ message: "Internal Server Error", error: error.message })
-    }
+    return res.status(200).json(summary)
 })
--- schemas/leaderboard-schema.js
+++
@@ -1,8 +0,0 @@
-import Joi from "joi"
-
-export const leaderboardSchema = Joi.object({
-    username: Joi.string().max(50).required(),
-    country: Joi.string().required(),
-    score: Joi.number().integer().min(0).required(),
-    dateCreated: Joi.string().required(),
-})
