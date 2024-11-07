9b695bfbbe094c76acfcb99d850a40f4ac45f103
Sofia Ex <dev.exarchou@outlook.com>
2024-11-07 22:31:19 +0100

    leaderboard controller

--- routes/leaderboard-routes.js
+++ routes/leaderboard-routes.js
@@ -1,51 +1,6 @@
 import { Router } from "express"
-import { BattleOutcome } from "../models/battle-outcome.js"
+import { getLeaderboard } from "../controllers/leaderboard-controller.js"
 
 export const leaderboardRouter = Router()
 
-leaderboardRouter.get("/", async (_, res) => {
-    const summary = await BattleOutcome.aggregate([
-        {
-            $facet: {
-                redPlayers: [
-                    {
-                        $project: {
-                            userId: "$playerRed.userId",
-                            score: "$playerRed.score",
-                        },
-                    },
-                ],
-                bluePlayers: [
-                    {
-                        $project: {
-                            userId: "$playerBlue.userId",
-                            score: "$playerBlue.score",
-                        },
-                    },
-                ],
-            },
-        },
-        {
-            $project: {
-                allPlayers: { $concatArrays: ["$redPlayers", "$bluePlayers"] },
-            },
-        },
-        { $unwind: "$allPlayers" },
-        {
-            $group: {
-                _id: "$allPlayers.userId",
-                totalScore: { $sum: "$allPlayers.score" },
-            },
-        },
-        {
-            $project: {
-                _id: 0,
-                userId: "$_id",
-                score: "$totalScore",
-            },
-        },
-        { $sort: { score: -1 } },
-    ])
-
-    return res.status(200).json(summary)
-})
+leaderboardRouter.get("/", getLeaderboard)
