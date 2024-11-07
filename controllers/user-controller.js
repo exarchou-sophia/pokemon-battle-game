81a153ccc7bbc32696807722b0cbea1a4c6a2250
Sofia Ex <dev.exarchou@outlook.com>
2024-11-07 22:14:57 +0100

    user controller file

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -5,6 +5,22 @@ export const getUsers = async (req, res) => {
     return res.status(200).json(users)
 }
 
+export const getUserById = async (req, res) => {
+    try {
+        const user = await User.findById(req.params.id).exec()
+
+        if (user) {
+            return res.status(200).json(user)
+        } else {
+            return res.status(404).json({ message: "User not found" })
+        }
+    } catch (error) {
+        return res
+            .status(500)
+            .json({ message: "Internal Server Error", error: error.message })
+    }
+}
+
 export const deleteUserById = async (req, res) => {
     try {
         const result = await User.deleteOne({ _id: req.params.id }).exec()
@@ -18,5 +34,56 @@ export const deleteUserById = async (req, res) => {
         return res
             .status(500)
             .json({ message: "Internal Server Error", error: error.message })
+    }
+}
+
+export const createUser = async (req, res) => {
+    const user = req.body
+
+    try {
+        const newUser = new User({
+            ...user,
+        })
+        await newUser.save()
+
+        return res.status(201).json({ id: newUser._id })
+    } catch (error) {
+        return res
+            .status(500)
+            .json({ message: "Internal Server Error", error: error.message })
+    }
+}
+
+export const addFavPokemon = async (req, res) => {
+    const userId = req.params.id
+    const { pokemonId } = req.body
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            { $addToSet: { favPokemonIds: pokemonId } },
+            { new: true },
+        )
+        res.status(200).json(updatedUser)
+    } catch (error) {
+        res.status(500).json({
+            error: "Failed to add Pokémon to favorites",
+        })
+    }
+}
+
+export const removeFavPokemon = async (req, res) => {
+    const userId = req.params.id
+    const { pokemonId } = req.body
+    try {
+        const updatedUser = await User.findByIdAndUpdate(
+            userId,
+            { $pull: { favPokemonIds: pokemonId } },
+            { new: true },
+        )
+        res.status(200).json(updatedUser)
+    } catch (error) {
+        res.status(500).json({
+            error: "Failed to remove Pokémon from favorites",
+        })
     }
 }
--- routes/user-routes.js
+++ routes/user-routes.js
@@ -1,85 +1,27 @@
 import { Router } from "express"
 import { validateUser } from "../middlewares/validate-user.js"
 import { validateUpdateFavPokemon } from "../middlewares/validate-update-fav-pokemon.js"
-import { User } from "../models/user.js"
-import { deleteUserById, getUsers } from "../controllers/user-controller.js"
+import {
+    deleteUserById,
+    getUsers,
+    createUser,
+    addFavPokemon,
+    removeFavPokemon,
+    getUserById,
+} from "../controllers/user-controller.js"
 
 export const userRouter = Router()
 
 userRouter.get("/", getUsers)
-userRouter.get("/:id", deleteUserById)
+userRouter.get("/:id", getUserById)
 
-userRouter.delete("/:id", async (req, res) => {
-    try {
-        const result = await User.deleteOne({ _id: req.params.id }).exec()
+userRouter.delete("/:id", deleteUserById)
 
-        if (result.deletedCount === 0) {
-            return res.status(404).json({ message: "User not found" })
-        }
+userRouter.post("/", validateUser, createUser)
 
-        return res.status(200).json()
-    } catch (error) {
-        return res
-            .status(500)
-            .json({ message: "Internal Server Error", error: error.message })
-    }
-})
-
-userRouter.post("/", validateUser, async (req, res) => {
-    const user = req.body
-
-    try {
-        const newUser = new User({
-            ...user,
-        })
-        await newUser.save()
-
-        return res.status(201).json({ id: newUser._id })
-    } catch (error) {
-        return res
-            .status(500)
-            .json({ message: "Internal Server Error", error: error.message })
-    }
-})
-
-userRouter.put(
-    "/:id/add-fav-pokemon",
-    validateUpdateFavPokemon,
-    async (req, res) => {
-        const userId = req.params.id
-        const { pokemonId } = req.body
-        try {
-            const updatedUser = await User.findByIdAndUpdate(
-                userId,
-                { $addToSet: { favPokemonIds: pokemonId } },
-                { new: true },
-            )
-            res.status(200).json(updatedUser)
-        } catch (error) {
-            res.status(500).json({
-                error: "Failed to add Pokémon to favorites",
-            })
-        }
-    },
-)
-
+userRouter.put("/:id/add-fav-pokemon", validateUpdateFavPokemon, addFavPokemon)
 userRouter.put(
     "/:id/remove-fav-pokemon",
     validateUpdateFavPokemon,
-    async (req, res) => {
-        const userId = req.params.id
-        const { pokemonId } = req.body
-        try {
-            const updatedUser = await User.findByIdAndUpdate(
-                userId,
-                { $pull: { favPokemonIds: pokemonId } },
-                { new: true },
-            )
-            res.status(200).json(updatedUser)
-        } catch (error) {
-            res.status(500).json({
-                error: "Failed to remove Pokémon from favorites",
-            })
-        }
-    },
+    removeFavPokemon,
 )
